34d7047351c0dcd3d919e6ef20c514246d866263
Vít Zaoral <vit.zaoral@viaprogram.com>
2020-06-24 09:15:34 +0200

    Better magnetic controller alarm

--- lib/MagneticLockController/MagneticLockController.cpp
+++ lib/MagneticLockController/MagneticLockController.cpp
@@ -2,13 +2,25 @@
 
 MagneticLockController::MagneticLockController()
 {
-    pinMode(LOCK_A_PIN, INPUT);
-    pinMode(LOCK_B_PIN, INPUT);
-    pinMode(LOCK_C_PIN, INPUT);
 }
 
 void MagneticLockController::setData()
 {
+    // clear "wires memory"
+    // on long cables it works slowly, so set tu LOW and check if is HIGH
+    pinMode(LOCK_A_PIN, OUTPUT);
+    pinMode(LOCK_B_PIN, OUTPUT);
+    pinMode(LOCK_C_PIN, OUTPUT);
+
+    digitalWrite(LOCK_A_PIN, LOW);
+    digitalWrite(LOCK_B_PIN, LOW);
+    digitalWrite(LOCK_C_PIN, LOW);
+
+    delay(200);
+    pinMode(LOCK_A_PIN, INPUT);
+    pinMode(LOCK_B_PIN, INPUT);
+    pinMode(LOCK_C_PIN, INPUT);
+
     Serial.print("Magnetic lock A: ");
     setSensorData(&sensorA, LOCK_A_PIN);
     Serial.print("Magnetic lock B: ");
